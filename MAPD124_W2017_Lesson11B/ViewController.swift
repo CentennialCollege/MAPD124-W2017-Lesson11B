09d7ca443c6aea1e93455a3f91dc8d63dd6cec71
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-03-28 21:18:19 -0400

    multi swipe

--- MAPD124_W2017_Lesson11B/ViewController.swift
+++ MAPD124_W2017_Lesson11B/ViewController.swift
@@ -19,19 +19,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        for touchCount in 0..<5 {
+        
         let vertical = UISwipeGestureRecognizer(target: self, action: #selector(reportVerticalSwipe(_:)))
         vertical.direction = [.up, .down]
+        vertical.numberOfTouchesRequired = touchCount
         view.addGestureRecognizer(vertical)
         
         let horizontal = UISwipeGestureRecognizer(target: self, action: #selector(reportHorizontalSwipe(_:)))
         horizontal.direction = [.left, .right]
+        horizontal.numberOfTouchesRequired = touchCount
         view.addGestureRecognizer(horizontal)
+        }
     }
     
     
     func reportHorizontalSwipe(_ recognizer:UIGestureRecognizer) {
         messageLabel.text = "Horizontal Swipe Detected"
+        let count = descriptionForTouchCount(recognizer.numberOfTouches)
+        messageLabel.text = "\(count)-finger horizontal swipe detected"
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() +
             Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC), execute: {
                 self.messageLabel.text = ""
@@ -41,13 +47,30 @@ class ViewController: UIViewController {
     
     func reportVerticalSwipe(_ recognizer:UIGestureRecognizer){
         messageLabel.text = "Vertical Swipe Detected"
+        let count = descriptionForTouchCount(recognizer.numberOfTouches)
+        messageLabel.text = "\(count)-finger vertical swipe detected"
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() +
             Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC), execute: {
                 self.messageLabel.text = ""
         })
     }
     
-    
+    func descriptionForTouchCount(_ touchCount:Int)->String {
+        switch touchCount {
+        case 1:
+            return "Single"
+        case 2:
+            return "Double"
+        case 3:
+            return "Triple"
+        case 4:
+            return "Quadruple"
+        case 5:
+            return "Quintuple"
+        default:
+            return ""
+        }
+    }
     
     
     
