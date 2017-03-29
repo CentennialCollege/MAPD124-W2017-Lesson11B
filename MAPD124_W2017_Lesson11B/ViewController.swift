800089095bca975d0f9c69ea85a5994c0f3d8ecd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-03-28 20:46:17 -0400

    added custom horizontal / vertical swipe

--- MAPD124_W2017_Lesson11B/ViewController.swift
+++ MAPD124_W2017_Lesson11B/ViewController.swift
@@ -7,18 +7,50 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    private var gestureStartPoint: CGPoint!
+    private static let minimumGestureLength = Float(25.0)
+    private static let maximumVariance = Float(5)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            gestureStartPoint = touch.location(in: self.view)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first, let gestureStartPoint = self.gestureStartPoint {
+            let currentPosition = touch.location(in: self.view)
+            
+            let deltaX = fabsf(Float(gestureStartPoint.x - currentPosition.x))
+            let deltaY = fabsf(Float(gestureStartPoint.y - currentPosition.y))
+            
+            if deltaX >= ViewController.minimumGestureLength && deltaY <= ViewController.maximumVariance {
+                messageLabel.text = "horizontal swipe detected"
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() +
+                    Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC), execute: {
+                        self.messageLabel.text = ""
+                })
+            }
+            else if deltaY >= ViewController.minimumGestureLength && deltaX <= ViewController.maximumVariance {
+                messageLabel.text = "vertical swipe detected"
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() +
+                    Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC), execute: {
+                        self.messageLabel.text = ""
+                })
+
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
